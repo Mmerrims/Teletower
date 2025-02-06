074c83acc4ea8469e9a66cd78ce525355cbf8b16
VideoDrane <cdrane@mail.bradley.edu>
2025-02-06 04:22:34 -0600

    Art Update
    Title, blood, and updated blocks in assets but not implemented. Teleport sfx implemented.

--- Assets/CamScripts/ParticleWiggle.cs
+++ Assets/CamScripts/ParticleWiggle.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class ParticleWiggle : MonoBehaviour
@@ -8,11 +9,17 @@ public class ParticleWiggle : MonoBehaviour
     [SerializeField] private float _delay;
     [SerializeField] private float _transdownValue;
 
+    /// <summary>
+    /// Starting wiggle movement
+    /// </summary>
     private void Start()
     {
         Wiggle();
     }
 
+    /// <summary>
+    /// Move in a random direction at a random speed, after a delay pick a new direction and speed
+    /// </summary>
     private void Wiggle()
     {
         _rB2D.velocity = new Vector2(Random.Range(-_maxSpeed, _maxSpeed),
@@ -21,10 +28,18 @@ public class ParticleWiggle : MonoBehaviour
         Invoke("Wiggle", _delay);
     }
 
+    /// <summary>
+    /// Every time fixed update calls, the opacity is reduced by _transdownValue, at 0% opacity destroy object
+    /// </summary>
     private void FixedUpdate()
     {
         Color col = _sR.color;
         col.a -= _transdownValue;
         _sR.color = col;
+
+        if (col.a == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }

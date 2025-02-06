cc864c7a138e27b916ab2aa7e1270436d6a39038
Hollow-Cube4444 <159060858+ACardamone4@users.noreply.github.com>
2025-02-06 14:48:22 -0600

    block freeze & destroy

--- Assets/Scripts/Amber Scripts/Block Removal.cs
+++ Assets/Scripts/Amber Scripts/Block Removal.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class BlockRemoval : MonoBehaviour
 {
     [SerializeField] private float _deathTime;
+    [SerializeField] private Rigidbody2D _rigidbody;
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -11,6 +13,23 @@ public class BlockRemoval : MonoBehaviour
         {
             print("RemoveStart");
             StartCoroutine(Remove());
+        }
+        if (collision.gameObject.tag == "BlockFreeze")
+        {
+            _rigidbody.constraints = RigidbodyConstraints2D.FreezePositionY;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BlockRemove")
+        {
+            print("RemoveStart");
+            StartCoroutine(Remove());
+        }
+        if (collision.gameObject.tag == "BlockFreeze")
+        {
+            _rigidbody.constraints = RigidbodyConstraints2D.FreezePositionY;
         }
     }
 

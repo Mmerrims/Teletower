21d1d75d863d2e89efb0d449c28ff548c5f097e6
YaelE25 <157845181+YaelE25@users.noreply.github.com>
2025-02-13 16:11:08 -0600

    EscapeTutorial

--- Assets/Scripts/Amber Scripts/Menu Controls.cs
+++ Assets/Scripts/Amber Scripts/Menu Controls.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
     [SerializeField] private GameObject MainMenuObjects;
     [SerializeField] private GameObject ControlsObjects;
+    public PlayerInput MPI;
+    private InputAction back;
+
+    private void Awake()
+    {
+        MPI = GetComponent<PlayerInput>();
+        back = MPI.currentActionMap.FindAction("Back");
+        MPI.currentActionMap.Enable();
+        back.started += Back;
+    }
+
+    public void OnDisable()
+    {
+        MPI.currentActionMap.Disable();
+        back.started -= Back;
+    }
 
     public void Quit()
     {
@@ -36,6 +53,12 @@ public class MenuControls : MonoBehaviour
     }
 
     public void HideControls()
+    {
+        MainMenuObjects.SetActive(true);
+        ControlsObjects.SetActive(false);
+    }
+
+    public void Back(InputAction.CallbackContext obj)
     {
         MainMenuObjects.SetActive(true);
         ControlsObjects.SetActive(false);

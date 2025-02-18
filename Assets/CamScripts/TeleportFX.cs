d1fc04f7b94fe883de79d3dcafa43acae9c226d2
VideoDrane <cdrane@mail.bradley.edu>
2025-02-18 13:22:31 -0600

    UpdatedTeleFX

--- Assets/CamScripts/TeleportFX.cs
+++ Assets/CamScripts/TeleportFX.cs
@@ -6,10 +6,12 @@ public class TeleportFX : MonoBehaviour
     [SerializeField] private GameObject _pX;
     [SerializeField] private GameObject _pY;
     [SerializeField] private GameObject _pZ;
+    [SerializeField] private GameObject _circle;
     [SerializeField] private Transform _playerTransform;
 
     public void TeleportFXSpawn()
     {
+        Instantiate(_circle, _playerTransform.position, Quaternion.identity);
         Instantiate(_pX, _playerTransform.position, Quaternion.identity);
         Instantiate(_pX, _playerTransform.position, Quaternion.identity);
         Instantiate(_pY, _playerTransform.position, Quaternion.identity);
